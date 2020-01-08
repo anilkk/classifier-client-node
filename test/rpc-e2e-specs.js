4c31e8af91562b8856f3993e7e62f29f7e4a1900
Anil Kumar krishanshetty <anilbms75@gmail.com>
2020-01-08 11:31:08 +0100

    Update spec test

--- test/rpc-e2e-specs.js
+++ test/rpc-e2e-specs.js
@@ -1,5 +1,3 @@
-import fs from 'fs';
-import path from 'path';
 import chai from 'chai';
 import should from 'should';
 import { remote } from 'webdriverio';
@@ -9,40 +7,9 @@ chai.use(should);
 
 const PORT = 50051;
 const HOST = "127.0.0.1";
-const FIXTURES = path.resolve(__dirname, "..", "..", "test", "fixtures");
-const CART_IMG = path.resolve(FIXTURES, "cart.png");
-const MIC_IMG = path.resolve(FIXTURES, "microphone.png");
-//const FOLDER_IMG = path.resolve(FIXTURES, "folder.png");
-//const MENU_IMG = path.resolve(FIXTURES, "menu.png");
-//const TINY_MENU_IMG = path.resolve(FIXTURES, "menu_small.png");
 
 describe('RPC server', function () {
-  it('should handle requests to classify elements by image', async function () {
-    const c = new ClassifierClient({host: HOST, port: PORT});
-    const input = {
-      labelHint: 'cart',
-      elementImages: {
-        cart: fs.readFileSync(CART_IMG),
-        mic: fs.readFileSync(MIC_IMG)
-      },
-      confidenceThreshold: 0.0,
-      allowWeakerMatches: true
-    };
-    const res = await c.classifyElements(input);
-    res.cart.label.should.eql('cart');
-    res.cart.confidence.should.be.above(0.9);
-    res.cart.confidence.should.be.below(1.0);
-    res.cart.confidenceForHint.should.be.above(0.9);
-    res.cart.confidenceForHint.should.be.below(1.0);
-
-    res.mic.label.should.eql('microphone');
-    res.mic.confidence.should.be.above(0.2);
-    res.mic.confidence.should.be.below(1.0);
-    res.mic.confidenceForHint.should.be.above(0.0);
-    res.mic.confidenceForHint.should.be.below(0.2);
-  });
-
-  it('should find elements when given a webdriverio object', async function () {
+  it('should find elements when given a webdriverio object', async function (done) {
     const c = new ClassifierClient({host: HOST, port: PORT});
     const driver = await remote({
       host: '127.0.0.1',
@@ -55,12 +22,13 @@ describe('RPC server', function () {
       await driver.url('https://test.ai');
       const els = await c.findElementsMatchingLabel({
         driver,
-        labelHint: 'twitter',
+        labelHint: 'blog',
       });
       await els[0].click();
-      (await driver.getUrl()).should.eql('https://twitter.com/testdotai');
+      (await driver.getUrl()).should.eql('https://www.test.ai/blog/');
     } finally {
       await driver.deleteSession();
+      done();
     }
   });
 });
